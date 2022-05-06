f4902030a10f8207a592c91fb43e616ea10bf567
lacepek <la.cepek@gmail.com>
2022-05-06 14:40:44 +0200

    inferred type

--- src/TranslationProvider.tsx
+++ src/TranslationProvider.tsx
@@ -1,16 +1,17 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, createContext } from "react";
 import { Options } from "translate.ts";
 
-import { Translations } from "./types";
-import { TranslationContext } from "./TranslationContext";
-
-export type TranslationProviderProps = {
-    translations: Translations;
+export type TranslationProviderProps<T = unknown> = {
+    translations: T;
     locale: string;
-    options: Options;
+    options?: Options;
 };
 
-export function TranslationProvider(props: PropsWithChildren<TranslationProviderProps>) {
+export const TranslationContext = createContext<TranslationProviderProps<unknown>>({ locale: "en", translations: {} });
+
+export function TranslationProvider<T = unknown>(props: PropsWithChildren<TranslationProviderProps<T>>) {
     const { translations, locale, children, options } = props;
-    return <TranslationContext.Provider value={{ translations, locale, options }}>{children}</TranslationContext.Provider>;
-};
+    return (
+        <TranslationContext.Provider value={{ translations, locale, options }}>{children}</TranslationContext.Provider>
+    );
+}
--- src/useTranslate.ts
+++ src/useTranslate.ts
@@ -1,13 +1,13 @@
 import { useContext } from "react";
 import { translate } from "translate.ts";
+import { TranslationContext } from "./TranslationProvider";
 
-import { TranslationContext } from "./TranslationContext";
 import { Params } from "./types";
 
 export function useTranslate(namespace = "") {
     const { translations, locale, options } = useContext(TranslationContext);
 
     return function (key: string, params?: Params) {
-        return translate(namespace ? `${namespace}.${key}` : key, locale, translations, params, options);
+        return translate(namespace ? `${namespace}.${key}` : key, locale, translations as any, params, options);
     };
 }
