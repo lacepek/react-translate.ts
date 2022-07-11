2f1d5f5e7d17894af559edd98a6228a6ca9008f6
lacepek <la.cepek@gmail.com>
2022-07-11 20:18:19 +0200

    translator, update

--- dist/useTranslate.d.ts
+++ dist/useTranslate.d.ts
@@ -1,2 +1,2 @@
 import { Params } from "./types";
-export declare function useTranslate(namespace?: string): (key: string, params?: Params | undefined) => any;
+export declare function useTranslate(namespace?: string): (key: string, params?: Params) => any;
